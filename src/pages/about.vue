5b4af273da47d172464046515fe18ca20e4d5078
Busayo Oyewole <busayomioyewole@gmail.com>
2020-10-26 14:10:21 +0100

    Create about.vue

---
+++ src/pages/about.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>About me</h1>
+    <img src="" alt="authors image">
+    <p>My name is Busayo - I'm a writer, designer, problem solver</p>
+    <h2>Writing else where</h2>
+    <ul>
+      <li>[Fiction]<a href="">Birds in flight</a></li>
+      <li>[Poetry]<a href="">The Poem</a></li> 
+      <li>[Essay]<a href="">Why poetry matters</a></li> 
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About'
+  }
+}
+</script>
